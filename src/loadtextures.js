0ef32c98a6082b1dc927aa56c39c422f5f4af571
Muyulu <142669199+MuyuluKaja@users.noreply.github.com>
2025-03-02 15:22:34 +0000

    CDN loadtextures.js
    Change to CDN imports

--- src/loadtextures.js
+++ src/loadtextures.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.140.0/build/three.module.js';
 
 // loader for terrain
 const loader = new THREE.TextureLoader();
