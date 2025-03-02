425c5eda0da3d954232fa6f9c80ace8724b1920f
Muyulu <142669199+MuyuluKaja@users.noreply.github.com>
2025-03-02 15:26:32 +0000

    Update loadtextures.js

--- src/loadtextures.js
+++ src/loadtextures.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.140.0/build/three.module.js';
+import * as THREE from 'three';
 
 // loader for terrain
 const loader = new THREE.TextureLoader();
